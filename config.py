759b5b85644f975c4d9e66f575cb6dcaf55c6211
chenjiandongx <chenjiandongx@qq.com>
2017-09-23 21:03:42 +0800

    调整各状态配置项

--- config.py
+++ config.py
@@ -7,9 +7,16 @@ class Config:
     配置基类
     """
     SECRET_KEY = '#+^aOjdlPHFD09)&*2P3JR-0CFE)&H12EAa;OPFG=0'
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = True        # 每次关闭会话都保存提交
-    SQLALCHEMY_TRACK_MODIFICATIONS = False      # 数据库变化追踪
-    FLASK_ADMIN = 'chenjiandongx@qq.com'        # 管理员邮箱
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_ADMIN = 'chenjiandongx@qq.com'
+    MAIL_USE_SSL = True
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASK_MAIL_SENDER = '简微博团队<chenjiandongx@qq.com>'
+    FLASK_MAIL_SUBJECT_PREFIX = '[简微博]'
 
 
 class DevelopmentConfig(Config):
@@ -17,16 +24,7 @@ class DevelopmentConfig(Config):
     开发状态
     """
     DEBUG = True
-    MAIL_USE_SSL = True
-    MAIL_SERVER = 'smtp.qq.com'
-    MAIL_PORT = 465
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
-    FLASK_MAIL_SENDER = '简微博团队'
-    FLASK_MAIL_SUBJECT_PREFIX = '[简微博]'
-
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(here, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(here, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
